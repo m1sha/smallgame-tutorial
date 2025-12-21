ba1ef400b1a22fb33727c1ee0daa64ce710d047d
m1sha <m1sha@live.ru>
2025-12-21 22:44:39 +0700

    search

--- src/components/example/components/script-list/script-list.vue
+++ src/components/example/components/script-list/script-list.vue
@@ -1,21 +1,34 @@
 <script setup lang="ts">
 import { computed, ref, watch } from 'vue';
-import { ItemList } from 'vue3-universal-components'
+import { ItemList, TextBox } from 'vue3-universal-components'
 
 const porps = defineProps<{ items: {id: string, name: string, category: string }[], selectIndex: number}>()
 const emit = defineEmits<{click: [id: number]}>()
-const categories = computed(() => new Set(porps.items.map(p => p.category)).keys())
+const filtredItems = computed(() => {
+  if (!search.value) return porps.items
+  return porps.items.filter(p => p.name.toLocaleLowerCase().includes(search.value.toLocaleLowerCase()))
+})
+const categories = computed(() => new Set(filtredItems.value.map(p => p.category)).keys())
 const selected = ref(porps.items[porps.selectIndex].id)
+const search = ref('')
 
 watch(() => porps.selectIndex, () => selected.value = porps.items[porps.selectIndex].id)
 
 </script>
 <template>
   <div class="script-list-wrapper" style="">
+    <div class="search-box">
+    <TextBox placeholder="Search" v-model="search" search />
+    </div>
+    <div class="scroll-list">
+      
     <template v-for="cat in categories">
       <p class="category">{{ cat }}</p>
-      <ItemList :items="items.filter(p => p.category === cat)" v-model="selected" @click="({ id }) => emit('click', items.findIndex(p => p.id === id) )" />
+      <ItemList :items="filtredItems.filter(p => p.category === cat)" v-model="selected" @click="({ id }) => emit('click', items.findIndex(p => p.id === id) )" />
     </template>
+
+    <p v-if="!filtredItems.length && search">Empty Result</p>
+    </div>
     
   </div>
 </template>
@@ -27,11 +40,17 @@ watch(() => porps.selectIndex, () => selected.value = porps.items[porps.selectIn
   top: 0
   padding: 8px 16px 
   background-color: #3737377e
-  height: calc( 100vh -  100px)
   width: 10vw
-  overflow-y: auto
-
   box-shadow: inset -16px 0px 60px #4444444e, 16px 0px 20px #3636364e
+
+  .search-box
+    margin-bottom: 18px
+
+
+  .scroll-list
+    
+    height: calc( 100vh -  150px)
+    overflow-y: auto
 
   p.category
     padding: 0
