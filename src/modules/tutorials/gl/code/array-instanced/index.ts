921d68bdad2d9c3a007a1d6e55cc509a37e6b1d0
m1sha <m1sha@live.ru>
2026-04-06 21:21:00 +0700

    drawArraysInstanced

--- src/modules/tutorials/gl/code/array-instanced/index.ts
+++ src/modules/tutorials/gl/code/array-instanced/index.ts
@@ -1,4 +1,4 @@
-import { float, Game, GL, M33, mat3, Rect, Time, vec2, vec3 } from 'smallgame'
+import { Color, float, Game, GL, Rect, Time, vec2, vec3 } from 'smallgame'
 import vertex from './shaders/vert'
 import fragmnet from './shaders/frag'
 import { UIBuilder, type ScriptModule, type ScriptSettings } from "../../../../../components/example"
@@ -10,38 +10,37 @@ export default async ({ container, containerSize, fps }: ScriptSettings): Promis
   const glSurface = gl.toSurface()
   using _ = gl.createProgram(vertex, fragmnet, 'assemble-and-use')
 
-  
-
   const r1 = new Rect(10, 10, 100, 100).gl(containerSize, 'triangles')
   const r2 = new Rect(10, 110, 200, 100).gl(containerSize, 'triangles')
 
-  //const vbo = gl.vbo('static', 'float', { aPos: vec2 })
-  //vbo.push([
+  const vbo = gl.vbo('static', 'float', { aPos: vec2 })
+  vbo.push([
+   ...r1,
+   ...r2
+  ])
+
+  //const aPos = gl.pointerArray('aPos', vec2, 'float', 2)
+  //aPos.push([
   //  ...r1,
-  // 
   //  ...r2
   //])
 
-  const aPos = gl.pointerArray('aPos', vec2, 'float', 2)
-  aPos.push([
-    ...r1,
-    ...r2
-  ])
+  
 
   const aSize = gl.pointerArray('aColor', vec3, 'float', 3)
   aSize.push([
-    0.5, 0.5, 0.5,
-    0.1, 0.5, 0.8,
-    0.5, 0.1, 0.8,
+    ...Color.from("#474747").toArray(),
+    ...Color.from("#546f85").toArray(),
+    ...Color.from("#5c124c").toArray(),
   ])
   aSize.div(1)
 
 
-  const aShift = gl.pointerArray('aMat', mat3, 'float', 3)
+  const aShift = gl.pointerArray('aShift', vec2, 'float', 2)
   aShift.push([
-    ...M33.I.toArray(),
-    ...new M33().rotateSelf(40).toArray(),
-    ...new M33().rotateSelf(45).toArray()
+    0.1, -0.2,
+    0.3, -0.2,
+    0.5, -0.2,
   ])
   aShift.div(1)
 
--- src/modules/tutorials/gl/code/array-instanced/shaders/vert.ts
+++ src/modules/tutorials/gl/code/array-instanced/shaders/vert.ts
@@ -2,17 +2,15 @@ export default /*glsl*/`
 
 in vec2 aPos;
 in vec3 aColor;
-in vec3 aMat0;
-in vec3 aMat1;
-in vec3 aMat2;
+in vec2 aShift;
+
 out vec3 vColor;
 
 void main()
 {
-  mat3 aMat = mat3(aMat0, aMat1, aMat2);
-  vec3 pos = vec3(aPos, 1.0);
-  gl_Position = vec4(pos, 1.0);
-  vColor = aMat0;
+  vec2 pos = aPos + aShift;
+  gl_Position = vec4(pos, 0.0, 1.0);
+  vColor = aColor;
   //gl_PointSize = aSize;
 }
 
