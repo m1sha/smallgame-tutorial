8c3e4441b1ca7d06b23936e566bfe1148bd9a5bc
m1sha <m1sha@live.ru>
2026-04-11 13:32:58 +0700

    tex. exp fixed images pathes

--- src/modules/tutorials/gl/code/texure/index.ts
+++ src/modules/tutorials/gl/code/texure/index.ts
@@ -14,8 +14,8 @@ async function main (container: HTMLDivElement, width: number, height: number) {
   const ctx = glSurface.context
   ctx.createProgram(vertex, fragmnet, 'assemble-and-use')
 
-  const img = await loadImage('workflow.png')
-  const tv = await loadImage('tv.png')
+  const img = await loadImage('bedstead-20-df.png')
+  const tv = await loadImage('istockphoto-517188688-612x612.jpg')
 
   const tex = ctx.createTexture('u_sampler2D', img)
   
