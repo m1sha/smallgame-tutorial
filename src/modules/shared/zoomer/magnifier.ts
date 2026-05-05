323cbada4a699c2278aed5361d2097d20b3fab48
m1sha <m1sha@live.ru>
2026-05-05 20:35:30 +0700

    Magnifier added reset method

--- src/modules/shared/zoomer/magnifier.ts
+++ src/modules/shared/zoomer/magnifier.ts
@@ -1,7 +1,7 @@
 import { GMath } from "smallgame"
 
 export class Magnifier {
-  private step = 1
+  private step = 0
   private _value: number = 1
 
   constructor (private min: number = 0.1, private max: number = 8) {
@@ -10,6 +10,7 @@ export class Magnifier {
 
   byDelta (delta: number) {
     this.step -= Math.sign(delta)
+    console.log('step', this.step)
     GMath.clamp(this.step, 0, 9)
     let zoom = GMath.logZoom(this.step, 4, 1, 2)
     zoom = GMath.clamp(zoom, this.min, this.max)
@@ -17,4 +18,9 @@ export class Magnifier {
   }
 
   get zoom () { return this._value }
+
+  reset () {
+    this.step = 0
+    this._value = 1    
+  }
 }
